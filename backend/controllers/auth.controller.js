5a3e3cbd3cfc53703362942b3913fcc833c9e1f0
Ashfaque-calicut <ashfaquepalazhi@gmail.com>
2024-03-06 20:34:47 +0530

    signUp route complete

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
+import bcrypt from 'bcryptjs';
 import User from "../models/user.model.js";
+import genererateTokenAndSetCookie from '../utils/generateToken.js';
 
 export const signup=async(req,res)=>{
 try {
@@ -11,6 +13,8 @@ try {
     return res.status(400).json({error:"Username already exists"})
    }
    // Hash Password Here
+   const salt= await bcrypt.genSalt(10);
+   const hashedPassword=await bcrypt.hash(password,salt);
    //https://avatar-placeholder.iran.liara.run/
    const boyProfilePic= `https://avatar.iran.liara.run/public/boy?username=${username}`
    const girlProfilePic= `https://avatar.iran.liara.run/public/girl?username=${username}`
@@ -18,10 +22,13 @@ try {
    const newUser =new User({
     fullName,
     username,
-    password,
+    password:hashedPassword,
     gender,
     profilePic: gender ==="male" ? boyProfilePic : girlProfilePic
    })
+if(newUser){
+  // Generate JWT token Here
+  await genererateTokenAndSetCookie(newUser._id,res)
   await newUser.save();
 
   res.status(201).json({
@@ -29,7 +36,10 @@ try {
     fullName:newUser.fullName,
     username:newUser.username,
     profilePic:newUser.profilePic
-  })
+  });
+} else{
+  res.status(400).json({error:"Invalid User Data"})
+}
 } catch (error) {
     console.log("Error in signup controller",error.message);
   res.status(500).json({error:"Internal server error"})  
