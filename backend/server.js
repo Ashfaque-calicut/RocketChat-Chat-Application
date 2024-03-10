5f921cbd514c07994896e28b4f50f081373ad082
Ashfaque-calicut <ashfaquepalazhi@gmail.com>
2024-03-10 16:20:29 +0530

    create messageRoute

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 import  express  from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import bodyParser from'body-parser'
 import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
+
 import connectToMongoDb from './db/connectToMongoDb.js';
 
 const app=express();
 dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use("/api/auth",authRoutes)
-app.use(express.json());
+app.use("/api/messages",messageRoutes)
+
+
 
 const PORT=process.env.PORT ;
 // app.get('/',(req,res)=>{
